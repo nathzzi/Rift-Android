21d438a0b8fa78ccea084280349f15fdb469d21c
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-09 22:41:04 +0800

    Update encton.lua

--- scripts/encton.lua
+++ scripts/encton.lua
@@ -4,3 +4,10 @@ setreadonly(client, true);
 
 repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BatuKvi123/Encton/refs/heads/main/EnctonSource"))() 
+wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "Encton Android",
+    Text = "This cheat is powered by Rift Android",
+    Icon = "rbxassetid://135123682987938",
+    Duration = 5
+})
