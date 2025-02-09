779c5df9eea7f4fc5174811b7e244dc5f67ce22e
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-10 02:54:49 +0800

    Update zomex.lua

--- scripts/zomex.lua
+++ scripts/zomex.lua
@@ -1,6 +1,8 @@
 setreadonly(client, false);
 client.enableautoexec()
 setreadonly(client, true);
+print("test")
 
 repeat task.wait() until game:IsLoaded()
+print("test2")
 loadstring(game:HttpGet("https://zomex.lol/ui.lua"))()
