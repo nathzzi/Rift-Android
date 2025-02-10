ed183a23ca570651fd4d0cdc04dcb6fad62f7f64
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-11 00:50:54 +0800

    Update aurelia.lua

--- scripts/aurelia.lua
+++ scripts/aurelia.lua
@@ -1,6 +1,2 @@
-setreadonly(client, false);
-client.enableautoexec()
-setreadonly(client, true);
-
 repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/nyazs/AureliaUI/refs/heads/main/ui'))()
