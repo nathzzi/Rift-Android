047bc08a9d9d56da867c3a8c0b64d1147b95c714
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-11 00:51:17 +0800

    Update sigmasploit.lua

--- scripts/sigmasploit.lua
+++ scripts/sigmasploit.lua
@@ -1,6 +1,2 @@
-setreadonly(client, false);
-client.enableautoexec()
-setreadonly(client, true);
-
 repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ted291504/SigmaSploit-ui/refs/heads/main/ui",true))()
