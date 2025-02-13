fb48662a3eec94c4bf0d951dbc04b00acbd904e0
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-13 21:09:18 +0800

    Update sigmasploit.lua

--- scripts/sigmasploit.lua
+++ scripts/sigmasploit.lua
@@ -1,1 +1,3 @@
+repeat task.wait() until game:IsLoaded()
+game:GetService("Players").LocalPlayer:Kick("Sorry Diet but xdemon said 'GET OUT' to me")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ted291504/SigmaSploit-ui/refs/heads/main/ui",true))()
