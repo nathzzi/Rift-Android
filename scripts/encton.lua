9113d509f11370f66359767ae150bc9b2e4e9386
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-17 01:06:49 +0800

    Update encton.lua

--- scripts/encton.lua
+++ scripts/encton.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BatuKvi123/Encton/refs/heads/main/EnctonSource"))() 
+client.execute(game:HttpGet("https://raw.githubusercontent.com/BatuKvi123/Encton/refs/heads/main/EnctonSource"))
