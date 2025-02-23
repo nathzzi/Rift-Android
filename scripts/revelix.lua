392a1927b4f8354f43f173c1c537d99b0d8b14ed
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-23 22:01:56 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,1 +1,1 @@
-client.execute(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()
