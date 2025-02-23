2f0f5fcd786c5ba98c92514341ed6892c5324659
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-24 00:13:50 +0800

    Update falcon.lua

--- scripts/falcon.lua
+++ scripts/falcon.lua
@@ -1,1 +1,4 @@
+setreadonly(client, false);
+client.enableautoexec()
+setreadonly(client, true);
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nyazs/FalconUI/refs/heads/main/falconui"))() 
