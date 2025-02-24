0633211bfb9ff8290a4070f81c269837e53d8ba9
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-24 23:37:03 +0800

    Update cubix.lua

--- scripts/cubix.lua
+++ scripts/cubix.lua
@@ -1,7 +1,1 @@
-local player = game.Players.LocalPlayer
-while not player do
-    task.wait()
-    player = game.Players.LocalPlayer
-end
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV199/Cubix/refs/heads/main/Cubix_Ui.lua"))()
