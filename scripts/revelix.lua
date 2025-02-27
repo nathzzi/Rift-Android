2d1f06270706aea52407091810eb784678d68344
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-27 23:24:31 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()
+wait(3)
+game:GetService("Players").LocalPlayer:Kick("Revelix has been disabled. Blame Clock for his cockyness and ignorance!");
