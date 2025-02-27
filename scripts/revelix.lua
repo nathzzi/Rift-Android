42de83b6e700ad683ffd697109524c40e7c92df9
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-28 00:30:20 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,3 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()w
-wait(2)
-game:GetService("Players").LocalPlayer:Kick("Who said i can't update Clock? Stupid nigger xd");
