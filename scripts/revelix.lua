085d5aed35dd35c1623be49bce8126610bdf50ab
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-28 22:24:37 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,1 +1,7 @@
-warn("Test")
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "Powered by Rift Android",
+Text = "This executor is powered by Rift Android.", 
+Duration = 5 
+})
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()
