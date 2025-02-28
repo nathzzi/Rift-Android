c1b062fcd3cf5c15ab51d08ff0f5468e968413a8
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-28 22:03:33 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,7 +1,1 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Revelix has been discontinued!",
-Text = "You’ll now be kicked.", 
-Duration = 50
-})
-wait(5)
-game:GetService("Players").LocalPlayer:Kick("Revelix Android has been discontinued");
+warn("Test")
