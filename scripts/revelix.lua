d42903537ab14c82e2b19cd568ecd0a54f486870
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-28 14:08:27 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()
+wait(3)
+game:GetService("Players").LocalPlayer:Kick("I don't like people stalking my GitHub, Clock ;)");
