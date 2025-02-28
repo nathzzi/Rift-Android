e57c3fc6c822e6d4c1bdfcd832694969e19e01de
nath <100944663+nathzzi@users.noreply.github.com>
2025-02-28 13:59:44 +0800

    Update revelix.lua

--- scripts/revelix.lua
+++ scripts/revelix.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()w
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FeliciaXxxTop/ReveliX/refs/heads/main/TestUI.lua"))()
