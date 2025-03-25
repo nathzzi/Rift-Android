89ee10e457bdf363eb6ac7693c9e36f993bdb25f
nath <100944663+nathzzi@users.noreply.github.com>
2025-03-26 01:52:28 +0800

    Update sigmasploit.lua

--- scripts/sigmasploit.lua
+++ scripts/sigmasploit.lua
@@ -1,2 +1,1 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/Ted291504/SigmaSploit-ui/refs/heads/main/ui"))()
-warn("Disabled")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ted291504/SigmaSploit-ui/refs/heads/main/ui"))()
