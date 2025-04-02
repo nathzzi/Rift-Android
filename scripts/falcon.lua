ea415dbd971a75540151e2403c38dda78513bd1a
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-04-02 23:43:39 +0800

    Delete scripts/falcon.lua

--- scripts/falcon.lua
+++
@@ -1,4 +0,0 @@
-setreadonly(client, false);
-client.enableautoexec()
-setreadonly(client, true);
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nyazs/FalconUI/refs/heads/main/falconui"))() 
