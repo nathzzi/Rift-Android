4eb67ac633da090202d5415071280322b193ff7e
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-04-05 00:05:51 +0800

    Update encton.lua

--- scripts/encton.lua
+++ scripts/encton.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BatuKvi123/EnctonKeySystem/refs/heads/main/KeySystem"))()
+game:GetService("Players").LocalPlayer:Kick("Encton has been disabled by Rift Management, Download Rift instead at https://getrift.lol/");
+setclipboard("https://getrift.lol")
